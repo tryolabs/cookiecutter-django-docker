926170d25f17f4be1e08d2c05cf9641470f6ebb9
Alan Descoins <dekked@gmail.com>
2015-08-04 18:56:14 -0300

    Switch nginx to dekked/nginx-docker

--- {{ cookiecutter.repo_name }}/deploy/docker/containers/nginx/docker-entrypoint-init.d/set-dev-hostname.sh
+++ {{ cookiecutter.repo_name }}/deploy/docker/containers/nginx/docker-entrypoint-init.d/set-dev-hostname.sh
@@ -4,9 +4,3 @@ then
     # Put host's IP in /etc/hosts so nginx can use it as server_name.
     echo "$(ip route|awk '/default/ { print $3 }') {{ cookiecutter.local_hosts_name }}" >> /etc/hosts
 fi
-
-cd /templates
-j2 nginx.conf.j2 > /etc/nginx/nginx.conf
-cd /etc/
-
-exec "$@"
